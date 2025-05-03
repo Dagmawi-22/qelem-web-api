a5451bda3968eb97960ee6a35a6dd267a7e2d64f
Dagmawi-22 <dagtt316@gmal.com>
2025-05-03 21:02:25 +0300

    remove unnecessary modules from template

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,17 +1,8 @@
 import { Module } from '@nestjs/common';
 import { AuthModule } from './auth/auth.module';
-import { OnboardingModule } from './onboarding/onboarding.module';
-import { CategoryModule } from './category/category.module';
-import { RatingModule } from './rating/rating.module';
-
 
 @Module({
-  imports: [
-    AuthModule,
-    OnboardingModule,
-    CategoryModule,
-    RatingModule,
-  ],
+  imports: [AuthModule],
   providers: [],
 })
 export class AppModule {}
--- src/onboarding/dto/bulk-question.dto.ts
+++
@@ -1,7 +0,0 @@
-import { OmitType } from '@nestjs/swagger';
-import { CreateQuestionDto } from './create-question.dto';
-
-export class CreateQuestionWithoutQuestionnaireDto extends OmitType(
-  CreateQuestionDto,
-  ['questionnaireId', 'userType', 'dependsOnAnswerValue', 'dependsOnQuestionId', 'orderIndex'] as const
-) {}
